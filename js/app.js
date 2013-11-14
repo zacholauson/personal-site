34f789b7356052b3dd1bb1e17fa96da93969650e
Zach Olauson <zmolauson@gmail.com>
2013-11-13 23:32:03 -0600

    initial site

--- js/app.js
+++ js/app.js
@@ -1,2 +1,11 @@
-$(document).ready(function(){
-})
+App = Ember.Application.create();
+
+App.Router.map(function() {
+  // put your routes here
+});
+
+App.IndexRoute = Ember.Route.extend({
+  model: function() {
+    return ['red', 'yellow', 'blue'];
+  }
+});
